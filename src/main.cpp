5e895a86831d3076dcd78f82cce016bb50f8e5e5
maghoff <unknown>
2011-03-31 17:00:17 +0200

    Whatever

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <boost/bind.hpp>
 #include <ymse/sdl_core.hpp>
 #include <ymse/game.hpp>
-#include <ymse/reshaper.hpp>
-#include <ymse/matrix.hpp>
-#include <ymse/matrix3d.hpp>
+#include <ymse/glviewport_reshaper.hpp>
+#include <ymse/box_reshaper.hpp>
+#include <ymse/matrix2d_homogenous.hpp>
 #include <ymse/vec.hpp>
 #include <ymse/bindable_keyboard_handler.hpp>
 #include <ymse/keycodes.hpp>
@@ -14,17 +14,39 @@ typedef ymse::vec<2, GLfloat> vec2f;
 typedef ymse::vec<3, GLfloat> vec3f;
 using ymse::matrix33f;
 
-class Game : public ymse::game, public ymse::reshaper {
+
+class Game : public ymse::game {
+	ymse::bindable_keyboard_handler keyboard;
+
+	ymse::box_reshaper box;
+	ymse::glViewport_reshaper viewport;
+
 	double ang;
-	matrix33f aspect;
 
 public:
+
+	Game() :
+		viewport(&box)
+	{
+	}
+
+	~Game() {
+	}
+
+	void attach_to_core(ymse::sdl_core& core) {
+		keyboard.bind_pressed(ymse::KEY_Q, boost::bind(&ymse::sdl_core::stop, &core, 0));
+		keyboard.bind_pressed(ymse::KEY_F, boost::bind(&ymse::sdl_core::toggle_fullscreen, &core));
+
+		core.set_game_object(this);
+		core.set_reshaper_object(&viewport);
+		core.set_keyboard_handler(&keyboard);
+	}
 
 	void render() {
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		using namespace ymse::matrix3d;
-		matrix33f m = aspect * rotate_z(ang) * scale(0.5);
+		using namespace ymse::matrix2d::homogenous;
+		matrix33f m = box.get_transformation() * rotate(ang) * scale(0.5);
 
 		vec2f pts[] = {
 			vec2f(-1, -1),
@@ -35,19 +57,14 @@ public:
 
 		glBegin(GL_LINE_LOOP);
 		for (unsigned i=0; i<sizeof(pts)/sizeof(*pts); ++i) {
-			vec3f v = m * vec3f(pts[i][0], pts[i][1], 0);
+			vec3f v = m * vec3f(pts[i][0], pts[i][1], 1);
 			glVertex2f(v[0], v[1]);
 		}
 		glEnd();
 	}
 
-	void tick() {
+	void tick_10ms() {
 		ang += 0.01;
-	}
-
-	void reshape(int width, int height) {
-		glViewport(0, 0, width, height);
-		aspect = ymse::matrix3d::scale(height/(double)width, 1, 1);
 	}
 };
 
@@ -57,13 +74,7 @@ int main(int argc, char** argv) {
 	core.init(argc, argv);
 
 	Game game;
-	ymse::bindable_keyboard_handler keyboard;
-
-	keyboard.bind(ymse::KEY_Q, boost::bind(&ymse::sdl_core::stop, &core, 0));
-
-	core.set_game_object(&game);
-	core.set_reshaper_object(&game);
-	core.set_keyboard_handler(&keyboard);
+	game.attach_to_core(core);
 
 	return core.run();
 }
