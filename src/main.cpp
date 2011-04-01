760bc242c09864a6a5c9037b4bd81d2c1b73e223
maghoff <unknown>
2011-04-01 19:06:17 +0200

    ELMA: WATCH OUT

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,64 @@
 #include <ymse/game.hpp>
 #include <ymse/bindable_keyboard_handler.hpp>
 #include <ymse/keycodes.hpp>
+#include <ymse/key.hpp>
 #include <ymse/box_reshaper.hpp>
 #include <ymse/matrix2d_homogenous.hpp>
 #include <ymse/vec.hpp>
+
+#include <Box2D.h>
 
 
 class Game : public ymse::game {
 	ymse::bindable_keyboard_handler keyboard;
 	ymse::box_reshaper box;
 
-	double ang;
+	ymse::key acc;
+
+	b2World world;
+	b2Body *groundBody, *body;
+
+	void createGroundBox() {
+		b2BodyDef groundBodyDef;
+		groundBodyDef.position.Set(0, -10);
+
+		groundBody = world.CreateBody(&groundBodyDef);
+
+		b2PolygonShape groundBox;
+		groundBox.SetAsBox(50, 10);
+
+		groundBody->CreateFixture(&groundBox, 0);
+	}
+
+	void createDynamicBody() {
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.position.Set(0, 4);
+		bodyDef.angle = 1.1;
+		body = world.CreateBody(&bodyDef);
+
+		b2PolygonShape dynamicBox;
+		dynamicBox.SetAsBox(1, 1);
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &dynamicBox;
+		fixtureDef.density = 1;
+		fixtureDef.friction = 0.3;
+
+		body->CreateFixture(&fixtureDef);
+	}
 
 public:
-	Game() { }
+	Game() :
+		acc(keyboard, ymse::KEY_UP),
+		world(b2Vec2(0, -10), true)
+	{
+		box.set_box(-1, 0, 1, 10);
+
+		createGroundBox();
+		createDynamicBody();
+	}
+
 	~Game() { }
 
 	void attach_to_core(ymse::sdl_core& core) {
@@ -35,7 +80,11 @@ public:
 
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		matrix33f m = box.get_transformation() * rotate(ang) * scale(0.5);
+		matrix33f m =
+			box.get_transformation() *
+			translate(body->GetPosition().x, body->GetPosition().y) *
+			rotate(-body->GetAngle())
+		;
 
 		vec2f pts[] = {
 			vec2f(-1, -1),
@@ -53,7 +102,14 @@ public:
 	}
 
 	void tick_10ms() {
-		ang += 0.01;
+		//ang += 0.01;
+
+		if (acc.val()) body->ApplyTorque(-50.);
+
+		float32 timeStep = 10. / 1000.;
+		int32 velocityIterations = 10;
+		int32 positionIterations = 10;
+		world.Step(timeStep, velocityIterations, positionIterations);
 	}
 };
 
